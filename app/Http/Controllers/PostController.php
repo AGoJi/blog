dfd9e6850c753e9c279b0fbb9046ef88c9a8ea26
Albert Gomicia <albertgomicia@gmail.com>
2021-11-06 23:23:46 +0100

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()
+                ->filter(request(['search', 'category', 'author']))
+                ->paginate(6)
+                ->withQueryString() //paginate() or simplePaginate() for pagination, withQueryString will include the current query string to the pagination
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+}
